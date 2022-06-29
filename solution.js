84ac35e9dca50c38b7703272520678c2459314cd
xstartstaticx <daniel.reiller@icloud.com>
2022-06-29 13:09:35 +0200

    finished exercise

--- solution.js
+++ solution.js
@@ -1,1 +1,19 @@
 // write your code in this file
+console.log("This is an exercise in console logging");
+
+const exercise = "This is the value.";
+console.log(exercise);
+
+const firstName = "Danielson";
+const lastName = "Reiller";
+console.log(firstName, lastName);
+
+const city = "Barmbek";
+const job = "Web Developer";
+const age = 35;
+console.log(firstName, lastName, city, job, age);
+
+console.log(
+  firstName,
+  lastName + " is a " + age + " year old " + job + " who lives in " + city
+);
